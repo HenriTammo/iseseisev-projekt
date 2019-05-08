94bf37f99a75624f6739d4eacfee6af7a4a4d7f1
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2019-05-08 10:34:26 +0300

    naaseb avalehele nupp lisatud

--- newuser.php
+++ newuser.php
@@ -1,51 +1,39 @@
-﻿<?php
+<?php
 	require("functions.php");
-
 	echo "<body style='background-color:#202020'>";
-
 	$email = "";
 	$name = "";
-
 	//muutujad võimalike veateadetega
 	$nameError = "";
 	$emailError = "";
 	$passwordError = "";
 	$error = "";
-
 	//kui on uue kasutaja loomise nuppu vajutatud
 	if(isset($_POST["submitUserData"])){
-
 	//var_dump($_POST); //Tähtis array $_POST
-
 	if (isset($_POST["firstName"]) and !empty($_POST["firstName"])){
 		//$name=$_POST["firstName"];
 		$name = test_input($_POST["firstName"]);
 	}else{
 		$nameError="Palun sisesta eesnimi!";
 	}
-
 	if(isset($_POST["email"]) and !empty($_POST["email"])){
 		$email = test_input($_POST["email"]);
 	}else{
 		$emailError = "Sisestage korrektne email!";
 	}
-
 	//passwordi kontroll
-
 	if(strlen($_POST["password"]) < 8){
 		$passwordError = "Salasõna peab olema vähemalt 8 tähemärgi pikkune";
 	}
 	//kui kõik on korras, siis salvestame kasutaja
-
 	if(empty($nameError) and empty($emailError) and empty($passwordError)){
 		$notice = signup($name ,$email, $_POST["password"]);
 		echo $notice;
 	}else{
 		$error = "Kasutaja loomisel tekkis viga!";
 	}
-
 	}//kui on nuppu vajutatud lõppeb ära
-
 ?>
 
 <!DOCTYPE html>
@@ -65,7 +53,10 @@
 			<input type="email" name="email" placeholder="E-Mail" value="<?php echo $email; ?>"><br>
 			<input type="password" name="password" type="text" placeholder="Salasõna" >
 			<br>
-			<input name="submitUserData" type="submit" value="Loo kasutaja" href="index.php">
+			<input name="submitUserData" type="submit" value="Loo kasutaja">
+			<br>
+			<br>
+			<a style="text-align:left;" href="index.php">Tagasi avalehele</a>
 		</form>
 		<a><?php echo $error; ?></a>
 	</div>
