7d5aeb9c80dd6e3a2129efc047177583e299ca87
kertliinat <43129546+kertliinat@users.noreply.github.com>
2019-05-12 19:40:19 +0300

    Kustutab, kuid kaustast mitte

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 /*jshint esversion:6*/
 function deactivate() {
     activeIMG.style.display='none';
-    console.log(document.getElementsByClassName("delBut"));
+    //console.log(document.getElementsByClassName("delBut"));
     $(".delBut").remove();
   }
 
-function createDeleteButton(){
-
-
-}
+  $(".delBut").on('tap', deletePic);
+  function deletePic(){
+    console.log("kaiperse");
+  }
 
 $(document).ready(function(){
   $('#list').css({'background-color':'#202020', 'border':'2px solid #202020', 'width':'80vw'});
@@ -22,11 +22,18 @@ $(document).ready(function(){
   let activeIMG = document.getElementById('activeIMG');
   let btn = document.createElement("BUTTON");
 
+
+
+
+
+
+
   $('img').click(function(){
     let currentPic = $('img.active');
     $(this).addClass('active');
     currentPic.removeClass('active');
     selectedPic = $(this).parent().find('.active').attr('src');
+
 
     $('li.list-element').html('<img src="' + selectedPic + '">').hide().fadeIn(2000);
     $('ul img').css({'width': '80vw', 'height':'80vh', 'margin-left':'7.5%'});
@@ -37,6 +44,15 @@ $(document).ready(function(){
 
     document.body.appendChild(btn);
     $("BUTTON").addClass("delBut");
+    $('.delBut').click(function(){
+      console.log("eieieieiei");
+      //siia pildi kustutamine
+      //document.getElementsByClassName("active");
+      //console.log(document.getElementsByClassName("active"));
+      $(".active").remove();
+      deactivate();
+
+    });
 
 
   });
